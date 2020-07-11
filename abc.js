56a4b9082a8b5b75f82fecf30eaef0c1eab4f176
Deep <deepdeys19933@gmail.com>
2020-07-11 11:54:38 +0000

    test

---
+++ abc.js
@@ -0,0 +1,11 @@
+myfunc2(p1,p2)
+{
+    var i,sum=0;
+    for (i=0; i<myfunc2.arguments.length ; i++)
+    {
+       sum1+=myfunc2.arguments[i] ;
+    }
+    console.log(myfunc2.arguments);
+    console.log(sum)
+
+}
